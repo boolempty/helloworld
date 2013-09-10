62105633870a76944f2b90e0faa7d815d394e575
boolempty <bool.empty@gmail.com>
2013-09-10 19:01:17 +0400

    rework

--- erlang/doc.erl
+++ erlang/doc.erl
@@ -1,5 +1,17 @@
 -module(doc).
--export([print_words_stats/1, lines_from_file/1, format/2]).
+-export([process_file_lines/2, print_words_stats/1, format/2, split/2, format_unstructured_text/2]).
+
+% ----------------------------------------------------------------
+process_file_lines(Fn, FnArgList, FileName) ->
+    {ok, F} = file:open(FileName, [read]),
+    process_file_lines_acc(file:read_line(F), Fn, FnArgList, F, []).
+
+process_file_lines_acc({ok, L}, Fn, FnArgList, F, R) when is_function(Fn) ->
+    process_file_lines_acc(file:read_line(F), Fn, FnArgList, F, [apply(Fn, [L|FnArgList])|R]);
+
+process_file_lines_acc(_ReadLineResult, _Fn, _FnArgList, F, R) ->
+    file:close(F),
+    lists:reverse(R).
 
 % ----------------------------------------------------------------
 from_line(Line) -> from_line_acc(Line, [], []).
@@ -19,31 +31,6 @@ from_line_acc([_Symbol|Line], [], Words) ->
 from_line_acc([_Symbol|Line], Word, Words) ->    
     from_line_acc(Line, [], [lists:reverse(Word)|Words]).
 
-% ----------------------------------------------------------------
-% TODO dup with lines_from_file_acc
-words_from_file(FileName) ->
-    {ok, File} = file:open(FileName, [read]),
-    words_from_file_acc(file:read_line(File), File, []).
-
-words_from_file_acc({ok, Line}, File, Words) ->
-    words_from_file_acc(file:read_line(File), File, [from_line(Line)|Words]);
-
-words_from_file_acc(_Result, File, Words) ->
-    file:close(File),
-    lists:reverse(Words).
-
-% ----------------------------------------------------------------
-lines_from_file(FileName) ->
-    {ok, File} = file:open(FileName, [read]),
-    lines_from_file_acc(file:read_line(File), File, []).
-
-lines_from_file_acc({ok, Line}, File, Lines) ->
-    lines_from_file_acc(file:read_line(File), File, [Line|Lines]);
-
-lines_from_file_acc(_Result, File, Words) ->
-    file:close(File),
-    lists:reverse(Words).    
-    
 % ----------------------------------------------------------------
 % Lines -- result of from_file/1.
 % ----------------------------------------------------------------
@@ -86,14 +73,14 @@ print([H|T])  -> print(H), io:format(","), print(T);
 print({N,M})  -> io:format("~p-~p", [N, M]); 
 print(N)      -> io:format("~p", [N]).
 print(W, N)   -> 
-    io:format("~s: ", [W]),
+    io:format("~s\t", [W]),
     print(make_ranges(drop_duplicates(lists:sort(N)))),
     io:format("~n").                    
     
 % ----------------------------------------------------------------
 % TODO make separate function 'get_words_stats'
 print_words_stats(FileName) ->
-    [{W,N}|T] = lists:keysort(1,flatten(words_from_file(FileName))),
+    [{W,N}|T] = lists:keysort(1,flatten(process_file_lines(fun from_line/1, FileName))),
     print_words_stats_acc(T, W, [N]).
           
 % next line number found       
@@ -110,36 +97,75 @@ print_words_stats_acc([{W1,N1}|T], W2, N2) ->
     print_words_stats_acc(T, W1, [N1]).
     
 % ----------------------------------------------------------------
-% split(Width, Words) -> split_acc(Width, Words, []).
-
-% split_acc(0, WordsOut, WordsIn) ->
-  %  {WordsIn, WordsOut};
-
-% split_acc(Width, [], WordsIn) ->
-  %   {WordsIn, []};
-    
-% split_acc(Width, [Word|OtherWords], WordsIn) when Width ->
-  %  split_acc(Width-length, [Word|OtherWords], [|WordsIn]);
-
-% split_acc(Width, [Word|OtherWords], WordsIn) ->
-  %  split_acc(Width-length, [Word|OtherWords], [|WordsIn]).
-    
 % ----------------------------------------------------------------
-% io:format("~s", [doc:format("doc.erl",10)])
 % ----------------------------------------------------------------
-format(FileName, Width) -> 
-    format_acc(Width, lines_from_file(FileName), []).
+   
+% ----------------------------------------------------------------
+% io:format("~s", [doc:format(10, "doc.erl")])
+% ----------------------------------------------------------------
+format(Width, FileName) -> 
+    % format_acc(Width, process_file_lines(fun (Line) -> Line end, FileName), []).
+    process_file_lines(fun set_line_width/2, [Width], FileName).
 
-format_acc(_Width, [], Result) ->       
+% format_acc(_Width, [], Result) ->       
+%     lists:reverse(Result);
+%     
+% % TODO where should be "\n"?    
+% format_acc(Width, [Line|Document], Result) when length(Line) > Width ->   
+%     {In, Out} = lists:split(Width, Line),
+%     format_acc(Width, ["\n",Out|Document], [In|Result]);
+% 
+% format_acc(Width, [Line|Document], Result) ->   
+%     format_acc(Width, Document, [Line|Result]).
+    
+% ----------------------------------------------------------------   
+set_line_width(Width, Line) -> 
+    set_line_width_acc(Width, Line, []).
+
+set_line_width_acc(_Width, [], Result) ->       
+    Result; % lists:reverse(Result);
+    
+% TODO where should be "\n"?    
+set_line_width_acc(Width, Line, Result) when length(Line) > Width ->   
+    {In, Out} = lists:split(Width, Line),
+    set_line_width_acc(Width, ["\n"|Out], Result ++ In);
+
+set_line_width_acc(Width, Line, Result) ->   
+    set_line_width_acc(Width, [], Result ++ Line).   
+  
+% ----------------------------------------------------------------
+% Format unstructured text.
+% ----------------------------------------------------------------
+split(Width, Words) -> split_acc(Width, [], Words).
+
+split_acc(_Width, WordsIn, []) ->
+    {lists:reverse(WordsIn), []};
+
+split_acc(0, WordsIn, WordsOut) ->
+    {lists:reverse(WordsIn), WordsOut};   
+    
+% If width is so small that even single word can't fit.                                        
+split_acc(Width, [], [W|WordsOut]) when Width =< length(W) ->
+    split_acc(0, [W], WordsOut);
+    
+split_acc(Width, WordsIn, [W|WordsOut]) when Width-length(W) >= 0 ->
+    split_acc(Width-length(W), [W|WordsIn], WordsOut);
+
+split_acc(_Width, WordsIn, WordsOut) ->
+    split_acc(0, WordsIn, WordsOut).
+       
+% ----------------------------------------------------------------
+format_unstructured_text(Width, FileName) -> 
+    format_unstructured_text_acc(Width, process_file_lines(fun from_line/1, FileName), []).
+
+format_unstructured_text_acc(_Width, [], Result) ->       
     lists:reverse(Result);
     
 % TODO where should be "\n"?    
-format_acc(Width, [Line|Document], Result) when length(Line) > Width ->   
-    {In, Out} = lists:split(Width, Line),
-    format_acc(Width, ["\n",Out|Document], [In|Result]);
-
-format_acc(Width, [Line|Document], Result) ->   
-    format_acc(Width, Document, [Line|Result]).
-
-    
+format_unstructured_text_acc(Width, [[]|Document], Result) ->   
+    format_unstructured_text_acc(Width, Document, [[]|Result]);     
+  
+format_unstructured_text_acc(Width, [Line|Document], Result) ->   
+    {In, Out} = split(Width, Line),    
+    format_unstructured_text_acc(Width, [Out|Document], [["\n"|In]|Result]). 
     
